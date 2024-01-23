4b4e857f4211684896444c930c06e872122e627e
Juan Q <jmquijano@unimayor.edu.co>
2024-01-22 20:39:18 -0500

    Fin Sección 9

---
+++ README.md
@@ -0,0 +1,7 @@
+# Proyecto NOC
+
+El objetivo es crear una serie de tareas usando Arquitectura Limpia con TypeScript
+
+# dev
+1. Clonar el archivo .env.template y renombrarlo a .env
+2. Configurar las variables de entorno
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,20 +1,30 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log-impl.repository";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource()
+);
+
+const URL = "https://www.google.com.co";
 
 export class ServerApp {
   static start() {
     CronService.createJob("*/5 * * * * *", () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log("success 1"),
         (error) => console.log(error)
-      ).execute("http://localhost:3000");
+      ).execute(URL);
     });
 
     CronService.createJob("*/60 * * * * *", () => {
       new CheckService(
+        fileSystemLogRepository,
         () => console.log("success 2"),
         (error) => console.log(error)
-      ).execute("https://google.com");
+      ).execute(URL);
     });
   }
 }
